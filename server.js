23d45f889cca66fe5777a5509e9528ff34c105a9
Anuragdubeyy <anurag.dubey16017@sakec.ac.in>
2025-09-29 00:48:12 +0530

    mongo server update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const MONGO = process.env.DB_PASSWORD;
 mongoose
   .connect(
-    `mongodb+srv://anuragdubey16017_db_user:${MONGO}@cluster0.1abym3w.mongodb.net/?appName=Manash_parivar`,
+    `mongodb+srv://anuragdubey16017_db_user:${MONGO}@cluster0.2yden43.mongodb.net/`,
   )
   .then(() => {
     console.log("Connected to MongoDB successfully");
