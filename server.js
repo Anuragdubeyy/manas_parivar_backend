f7ed100093b1ba1c2defa645da2b207029fd4cc9
Anuragbudeyy <anurag.dubey16017@sakec.ac.in>
2025-10-29 20:28:46 +0530

    sever

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:5173"],
+  origin: ["https://manas-parivar.vercel.app/","http://localhost:3000", "http://localhost:5173"],
   credentials: true,
 }));
 app.use(express.json());
